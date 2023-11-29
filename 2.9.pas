736c5d3f24020f4df85bd253fae9eccb36ea98a9
valeriyorlo <144387877+valeriyorlo@users.noreply.github.com>
2023-11-29 17:56:55 +0300

    Add files via upload

---
+++ 2.9.pas
@@ -0,0 +1,18 @@
+﻿const
+  arraySize = 20;
+
+var
+  arr: array[1..arraySize] of Integer;
+  i, firstPositiveIndex, minIndex: Integer;
+
+begin
+  Randomize;
+  for i := 1 to arraySize do
+    arr[i] := Random(100) - 50;
+  for i:=1 to arraySize do 
+  begin
+    if array[i] > 0:
+    
+    break;
+    end;
+  end;
