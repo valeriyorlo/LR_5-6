8a0c641160d60870af4c8821da5f458320110a2c
valeriyorlo <144387877+valeriyorlo@users.noreply.github.com>
2023-11-29 17:45:04 +0300

    Add files via upload

---
+++ 2.10.pas
@@ -0,0 +1,22 @@
+﻿var
+  a,b: array[1..20] of Integer;
+  i,c: Integer;
+begin
+  for i:= 1 to 20 do
+  begin
+    a[i]:= random (-15,15);
+    write (a[i],' ');
+    end;
+    writeln;
+    begin
+    c:= 1;
+    for i:= 1 to 20 do
+      if a[i] > 0 then
+        begin
+        b[c]:=a[i];
+        c:=c+1;
+      end;
+      for i:=1 to c - 1 do
+      write(' ',b[i]);
+end;
+end.
